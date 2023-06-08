1ebfb30ec3303963a0fd9c08cae15f6107a0ac94
almudenariojo <103251095+almudenariojo@users.noreply.github.com>
2023-06-08 12:57:19 +0200

    cambios

--- eventos.js
+++ eventos.js
@@ -2,35 +2,70 @@
 
 //eventos del raton
 
-function cambiarColor() {
-    var bg = boton.style.background;
-    if (bg == "green") {
-        boton.style.background = "red";
-         
-    } else {
-        boton.style.background = "green";
+window.addEventListener('load', () => {
+    function cambiarColor() {
+        var bg = boton.style.background;
+        if (bg == "green") {
+            boton.style.background = "red";
+             
+        } else {
+            boton.style.background = "green";
+        }
+        boton.style.padding = "10px";
+        boton.style.border = "1px solid #ccc";
+        return true;
     }
-    boton.style.padding = "10px";
-    boton.style.border = "1px solid #ccc";
-    return true;
-}
-
-var boton = document.querySelector("#boton");
-console.log("Me has dado click");
-
-//click
-boton.addEventListener('click', function(){
-    cambiarColor();
+    
+    var boton = document.querySelector("#boton");
+    console.log("Me has dado click");
+    
+    //click
+    boton.addEventListener('click', function(){
+        cambiarColor();
+    });
+    
+    //mouse over
+    
+    boton.addEventListener('mouseover', function(){
+        boton.style.background = "orange";
+    });
+    
+    //mouseout
+    
+    boton.addEventListener('mouseout', function(){
+        boton.style.background = "black";
+    });
+    
+    //Eventos
+    
+    var input = document.querySelector("#campo_nombre");
+    input.addEventListener('focus', function(){
+        console.log("Esta dentro del input");
+    });
+    
+    //Blur
+    
+    input.addEventListener('blur', function(){
+        console.log("Esta fuera del input");
+    });
+    
+    //KeyDown
+    
+    input.addEventListener('keydown', function(event){
+        console.log("Pulsando esta tecla", String.fromCharCode(event.keyCode));
+    });
+    
+    //Keypress
+    
+    input.addEventListener('keypress', function(event){
+        console.log("Tecla presionada", String.fromCharCode(event.keyCode));
+    });
+    
+    
+    //Keyup
+    
+    input.addEventListener('keyup', function(event){
+        console.log("Tecla soltada", String.fromCharCode(event.keyCode));
+    });
 });
-
-//mouse over
-
-boton.addEventListener('mouseover', function(){
-    boton.style.background = "orange";
-});
-
-//mouseout
-
-boton.addEventListener('mouseout', function(){
-    boton.style.background = "black";
-});
+// Final del load
