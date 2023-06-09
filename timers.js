3eb7b0e58b574ed495af1f5da08f9ef50170c1a7
almudenariojo <103251095+almudenariojo@users.noreply.github.com>
2023-06-09 15:18:14 +0200

    avance

--- timers.js
+++ timers.js
@@ -2,16 +2,37 @@
 
 window.addEventListener('load', function(){
 
-    //Timers
-    var tiempo = setInterval(function(){
+    function intervalo(){
+
+    var tiempo = this.setInterval(function(){ 
         console.log ("Set interval ejecutado");
    
     var encabezado = document.querySelector("h1");
     if (encabezado.style.fontSize ="50px"){
-        encabezado.style.fontSize ="20px";
+        encabezado.style.fontSize ="10px";
     }else{
         encabezado.style.fontSize ="50px";
     }
-},500);
+
+}, 1000);
+
+return tiempo;
+    }
+
+    var tiempo =intervalo()
+
+var stop = document.querySelector ("#stop");
+
+stop.addEventListener ("click", function(){
+    alert("Has parado el intervalo en bucle");
+clearInterval(tiempo);
+  });
+
+  var start = document.querySelector ("#start");
+
+start.addEventListener ("click", function(){
+    alert("Has iniciado el intervalo en bucle");
+clearInterval(tiempo);
+  });
 
 });
