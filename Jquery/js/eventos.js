c3669dcb5d6e8ce523868e9f935bbb9f5aa149c6
almudenariojo <103251095+almudenariojo@users.noreply.github.com>
2023-06-20 23:38:13 +0200

    include

---
+++ Jquery/js/eventos.js
@@ -0,0 +1,82 @@
+$(document).ready(function(){
+
+    //MouseOver y MouseOut
+
+    var caja = $("#caja"); // Selecciona el elemento con el id "caja" y lo asigna a la variable caja
+
+    /*
+    caja.mouseover(function(){
+        $(this).css("background", "red"); // Cuando el cursor pasa por encima de caja, cambia el fondo a rojo
+    });
+
+    caja.mouseout(function(){
+        $(this).css("background", "green"); // Cuando el cursor sale de caja, cambia el fondo a verde
+    });
+    */
+
+    //Hover
+
+    function cambiaAzul(){
+        $(this).css("background", "blue"); // Cambia el fondo a azul cuando se activa el evento hover en caja
+    }
+    
+    function cambiaRosa(){
+        $(this).css("background", "pink"); // Cambia el fondo a rosa cuando se desactiva el evento hover en caja
+    }
+    
+    caja.hover(cambiaAzul, cambiaRosa); // Asigna los manejadores de eventos cambiaAzul y cambiaRosa al evento hover de caja
+
+ //Click
+    
+ caja.click(function(){
+    $(this).css("background", "black"); // Cuando se hace clic en caja, cambia el fondo a negro
+    $(this).css("color", "white"); // Cuando se hace clic en caja, cambia el color del texto a blanco
+});
+
+//Double Click
+
+caja.dblclick(function(){
+    $(this).css("background", "yellow"); // Cuando se hace doble clic en caja, cambia el fondo a amarillo
+    $(this).css("color", "red"); // Cuando se hace doble clic en caja, cambia el color del texto a rojo
+});
+
+//Focus
+var nombre = $("#nombre"); // Selecciona el elemento con el id "nombre" y lo asigna a la variable nombre
+var datos = $("#datos");
+nombre.focus(function(){
+    $(this).css("background","green"); // Cuando el campo de entrada obtiene el foco, se establece un borde punteado verde de 2 píxeles
+    console.log("Entro"); // Imprime un mensaje en la consola cuando se activa el evento focus
+});
+
+nombre.blur(function(){
+    $(this).css("border","2px dashed black"); // Cuando el campo de entrada pierde el foco, se establece un borde punteado verde de 2 píxeles
+    console.log("Salio"); // Imprime un mensaje en la consola cuando se activa el evento blur
+    $("this").val();
+    datos.text($(this).val()).show();
+});
+
+//Mousedown y mouseup
+
+datos.mousedown(function(){ //Cuando presionas
+    $(this).css("border-color", "gray");
+});
+datos.mouseup(function(){ //Cuando presionas
+    $(this).css("border-color", "red");
+});
+
+//Mousemove
+
+var sigueme = $("#sigueme");
+
+$(document).mousemove(function(event){
+    console.log("Coordenadas de x: " + event.clientX);
+    console.log("Coordenadas de y: " + event.clientY);
+    sigueme.css("left", event.clientX);
+    sigueme.css("top", event.clientY);
+    $('body').css("cursor", "none");
+});
+
+
+
+
+});
--- Jquery/js/selectores.js
+++ Jquery/js/selectores.js
@@ -1,31 +1,67 @@
-
 $(document).ready(function(){
+    // Este código se ejecuta cuando el documento HTML ha cargado completamente
 
-    //Selector de ID
-    var rojo = $("#rojo").css("background", "red")
-                         .css("color", "white")
+    // Selector de ID
+    var rojo = $("#rojo").css("background", "red") // Selecciona el elemento con el ID "rojo" y cambia su fondo a rojo
+                         .css("color", "white"); // Cambia el color de texto a blanco
     console.log(rojo);
 
-    //Selector de ID
-    var amarillo = $("#amarillo").css("background", "yellow")
-                         .css("color", "green")
-   console.log(amarillo);
+    // Selector de ID
+    var amarillo = $("#amarillo").css("background", "yellow") // Selecciona el elemento con el ID "amarillo" y cambia su fondo a amarillo
+                                 .css("color", "green"); // Cambia el color de texto a verde
+    console.log(amarillo);
 
-   //Selector de ID
-   var verde= $("#verde").css("background", "green")
-   .css("color", "yellow")
+    // Selector de ID
+    var verde= $("#verde").css("background", "green") // Selecciona el elemento con el ID "verde" y cambia su fondo a verde
+                          .css("color", "yellow"); // Cambia el color de texto a amarillo
     console.log(verde);
 
-   //Selector de clase
-   $('.zebra').css("border", "5px dashed black");
-   var miClase =  $('.zebra');
-   miClase.css("border", "5px dashed blue");
-   console.log(miClase);
-   console.log(miClase.eq(1));
+    // Selector de clase
+    var miClase = $('.zebra').css("border", "5px dashed black"); // Selecciona todos los elementos con la clase "zebra" y les aplica un borde punteado de color negro
+    console.log(miClase);
+    console.log(miClase.eq(1)); // Muestra el segundo elemento de la colección de elementos seleccionados por la clase "zebra"
 
-   $('.sinBorde').click(function(){
-    console.log("Click dado")
-    $(this).addClass('zebra');
-   });
+    $('.sinBorde').click(function(){
+        // Cuando un elemento con la clase "sinBorde" es clicado
+        $(this).addClass('zebra'); // Añade la clase "zebra" al elemento clicado
+        console.log("Click dado");
+    });
+
+    // Selectores de etiquetas
+    var parrafos = $('p').css("cursor","pointer"); // Selecciona todos los elementos <p> y cambia el cursor del mouse a un puntero
+
+    parrafos.click(function(){
+        // Cuando un párrafo es clicado
+        var parr = $(this);
+
+        if(!parr.hasClass('grande')){
+            // Si el párrafo no tiene la clase "grande"
+            parr.addClass("grande"); // Añade la clase "grande" al párrafo
+        } else {
+            parr.removeClass('grande'); // Si el párrafo tiene la clase "grande", la remueve
+        }
+    });
+
+// Selectores de atributo
+$('[title="Google"]').css('background', '#ccc');
+$('[title="Facebook"]').css('background', 'blue');
+$('[title="Yahoo"]').css('background', 'yellow');
+$('[title="Twitter"]').css('background', 'orange');
+
+//Otros
+
+$('p,a').addClass("margenSuperior");
+
+// Búsqueda de elementos en el DOM
+var busqueda = $("#elemento1") // Selecciona el elemento con el ID "elemento1"
+                .parent() // Selecciona el elemento padre del elemento con el ID "elemento1"
+                .parent() // Selecciona el elemento padre del padre del elemento con el ID "elemento1"
+                .find('.resaltado') // Busca los elementos descendientes con la clase "resaltado"
+                .css("color", "blue"); // Cambia el color de texto de los elementos encontrados a azul
+
+console.log(busqueda); // Muestra en la consola los elementos encontrados
+
+
 
 });
+
