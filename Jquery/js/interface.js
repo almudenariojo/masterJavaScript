a729d8980d46254beb5599289a8c50e4d7844f4b
almudenariojo <103251095+almudenariojo@users.noreply.github.com>
2023-07-07 18:43:41 +0200

    subir

---
+++ Jquery/js/interface.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    //Mover elemnto por la página
+    $(".elemento").draggable();
+    //Redimensionar
+    $(".elemento").resizable();
+    //Seleccionar
+    $(".lista-seleccionable").selectable();
+})
