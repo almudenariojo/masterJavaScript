077895b11e8711a881c0d66b6f9e2c4af3b7701b
almudenariojo <103251095+almudenariojo@users.noreply.github.com>
2023-07-07 22:56:35 +0200

    mas

--- Jquery/js/interface.js
+++ Jquery/js/interface.js
@@ -3,6 +3,38 @@ $(document).ready(function(){
     $(".elemento").draggable();
     //Redimensionar
     $(".elemento").resizable();
-    //Seleccionar
-    $(".lista-seleccionable").selectable();
-})
+    //Seleccionar y ordenar elementos (Selectable y sortable no pueden ir juntos)
+   // $(".lista-seleccionable").selectable();
+    $(".lista-seleccionable").sortable({
+        update: function(event, iu){
+         console.log("Ha cambiado la lista");
+        }
+    });
+
+    // Drop
+    $("#elemento-movido").draggable();
+    $("#area").droppable({
+        drop: function(){
+            console.log("Has soltado algo dentro del área");
+        }
+    });
+
+    //Efectos
+    $("#mostrar").click(function(){
+        $(".caja-efectos").toggle("explode"); 
+        //Otros efectos: "blind", "slide", "drop", "fold", "puff", "escale", ("shake",4000) ..
+    });
+
+    //Tooltip
+    $(document).tooltip();
+
+    //Dialog
+    $("#lanzar-popup").click(function(){
+        $("#popup").dialog();
+    });
+
+    //Datepicker
+    $("#calendario").datepicker(); 
+
+  
+});
