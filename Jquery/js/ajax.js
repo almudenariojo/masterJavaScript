1bcd33c01e68d29fb2283f309828344c40d32415
almudenariojo <103251095+almudenariojo@users.noreply.github.com>
2023-07-07 15:13:53 +0200

    mas

--- Jquery/js/ajax.js
+++ Jquery/js/ajax.js
@@ -1,21 +1,59 @@
 $(document).ready(function(){
-    //Load
-    //$('#datos').load("https://reqres.in/");
+  //Load
+  //$('#datos').load("https://reqres.in/");
   
-    //Get y Post
-    $.get("https://reqres.in/api/users", { page: 2 }, function(response){
-      // Itera sobre los elementos en la propiedad 'data' del objeto 'response'
-      response.data.forEach(function(element, index) {
-        // Agrega un párrafo al elemento con el id 'datos'
-        $("#datos").append("<p>"+ element.first_name + " " + element.last_name +  "</p>");
-      });
+  //Obtener y Enviar (Get y Post)
+  $.get("https://reqres.in/api/users", { page: 2 }, function(response){
+    // Itera sobre los elementos en la propiedad 'data' del objeto 'response'
+    response.data.forEach(function(element, index) {
+      // Agrega un párrafo al elemento con el id 'datos' que muestra el nombre y apellido de cada usuario
+      $("#datos").append("<p>"+ element.first_name + " " + element.last_name +  "</p>");
     });
-var usuario = {
-    name : "Almudena Riojo",
-    web : "google.com"
-}   
- $.post("https://reqres.in/api/users", usuario, function(response){
-console.log(response);
- });
   });
+
+  // Maneja el envío del formulario
+  $("#formulario").submit(function(e){
+    e.preventDefault();
+    // Crea un objeto 'usuario' con los valores de los campos de entrada del formulario
+    var usuario = {
+      name: $('input[name="nombre"]').val(),
+      web: $('input[name="web"]').val()
+    };
+    console.log(usuario);
+
+    // Envía una solicitud POST al destino especificado en el atributo 'action' del formulario, enviando el objeto 'usuario'
+    /* 
+    $.post($(this).attr("action"), usuario, function(response){
+      console.log(response);
+    }).done(function(){
+      // Muestra una alerta cuando la solicitud se completa exitosamente
+      alert("Usuario añadido correctamente");
+    });
+    */
+
+    $.ajax({
+      type: "POST",
+      dataType: 'json',
+      url: $(this).attr("action"),
+      data: usuario,
+      beforeSend: function () {
+        // Antes de enviar la solicitud, muestra un mensaje en la consola
+        console.log("Enviando usuario ...");
+      }, 
+      success: function(response){
+        // Si la solicitud es exitosa, muestra la respuesta en la consola
+        console.log(response);
+      },
+      error: function(){
+        // Si ocurre un error durante la solicitud, muestra un mensaje en la consola
+        console.log("Ha ocurrido un error");
+      },
+      timeout: 2000 // Establece un límite de tiempo de espera para la solicitud (en milisegundos)
+    });
+    
+    return false;
+    });
+    
+});
+
   
